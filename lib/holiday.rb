1345cd4695134b1bb46939818f69c076b7b4a451
Tammy Lam <lamtammy1515@gmail.com>
2020-02-01 01:09:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,6 +72,15 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  new_array = []
+holiday_hash.each do |season, holiday|
+  holiday.each do |halliday, array| 
+    if array.include?("BBQ")
+      new_array << halliday
+      new_array.join(" ")
+    end
+  end
+end
 
 end
 
